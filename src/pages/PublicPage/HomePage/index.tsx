c8e6bcc211ab8cd7518cf140dfeeedaf9648e1ce
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-03-22 02:30:27 +0800

    Initialize Public Page

---
+++ src/pages/PublicPage/HomePage/index.tsx
@@ -0,0 +1,10 @@
+// ANCHOR React
+import * as React from 'react';
+
+// ANCHOR React Router
+
+// ANCHOR Models
+
+// ANCHOR Components
+
+export const HomePage = () => (<p>Home Page</p>);
---
+++ src/pages/PublicPage/index.tsx
@@ -0,0 +1,23 @@
+// ANCHOR React
+import * as React from 'react';
+
+// ANCHOR React Router
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// ANCHOR Global Provider
+import { GlobalProvider } from '../../components/GlobalProvider';
+
+// ANCHOR Pages
+import { HomePage } from './HomePage';
+
+export const PublicPage = () => (
+  <GlobalProvider>
+    <Router>
+      <Switch>
+        {/** SECTION */}
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/dashboard" component={HomePage} />
+      </Switch>
+    </Router>
+  </GlobalProvider>
+);
