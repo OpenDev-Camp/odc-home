d005874635a89212d4164b521a093d073958b3b0
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-03-22 02:00:06 +0800

    Initialize page

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { GlobalProvider } from './components/GlobalProvider';
+import { NotFoundPage } from './pages/ErrorPage/NotFoundPage';
+import { PublicPage } from './pages/PublicPage';
+
+export const App = React.memo(() => (
+  <GlobalProvider>
+    <Router>
+      <Switch>
+        {/* SECTION: Sign In Page  */}
+        <Route exact path="/public" component={PublicPage} />
+        {/* SECTION: 404 Page  */}
+        <Route component={NotFoundPage} />
+      </Switch>
+    </Router>
+  </GlobalProvider>
+));
