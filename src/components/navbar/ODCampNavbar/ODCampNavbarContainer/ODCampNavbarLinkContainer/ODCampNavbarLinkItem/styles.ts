b8df006bb6437be432ed3f1a14d7f258331e3be5
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-03-22 19:44:57 +0800

    Fix navbar link styling

--- src/components/navbar/ODCampNavbar/ODCampNavbarContainer/ODCampNavbarLinkContainer/ODCampNavbarLinkItem/styles.ts
+++ src/components/navbar/ODCampNavbar/ODCampNavbarContainer/ODCampNavbarLinkContainer/ODCampNavbarLinkItem/styles.ts
@@ -1,8 +1,12 @@
 import { makeStyles } from '@material-ui/core/styles';
 
+// ANCHOR Navbar link item margin
+const NAVBAR_LINK_ITEM_MARGIN = '40px';
+
 export const useStyles = makeStyles(() => ({
   root: {
     textDecoration: 'none',
     color: 'white',
+    marginLeft: NAVBAR_LINK_ITEM_MARGIN,
   },
 }));
--- src/components/navbar/ODCampNavbar/ODCampNavbarContainer/ODCampNavbarLinkContainer/styles.ts
+++ src/components/navbar/ODCampNavbar/ODCampNavbarContainer/ODCampNavbarLinkContainer/styles.ts
@@ -1,11 +1,12 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-// ANCHOR Navbar width
+// ANCHOR Navbar link container width
 const NAVBAR_LINK_CONTAINER_WIDTH = 'auto';
 
 export const useStyles = makeStyles(() => ({
   root: {
-    display: 'grid',
+    display: 'flex',
+    flexDirection: 'row',
     width: NAVBAR_LINK_CONTAINER_WIDTH,
   },
 }));
