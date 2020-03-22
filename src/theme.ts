12d2ca7b5dcd0f25b425eba993d12361d073d6af
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-03-22 19:33:51 +0800

    Change font to Nunito

--- src/theme.ts
+++ src/theme.ts
@@ -3,7 +3,7 @@ import { purple } from '@material-ui/core/colors';
 
 export const theme = createMuiTheme({
   typography: {
-    fontFamily: '"IBM Plex Sans", -apple-system, BlinkMacSystemFont, Helvetica, Arial, sans-serif',
+    fontFamily: '"Nunito", -apple-system, BlinkMacSystemFont, Helvetica, Arial, sans-serif',
   },
   palette: {
     primary: purple,
