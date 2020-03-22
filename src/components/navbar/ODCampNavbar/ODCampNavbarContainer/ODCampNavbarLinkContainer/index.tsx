2eba8d9fc481e080670ffb95164e749756d65403
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-03-22 23:04:27 +0800

    Add hamburger scoped model

--- src/components/navbar/ODCampNavbar/ODCampNavbarContainer/ODCampNavbarLinkContainer/index.tsx
+++ src/components/navbar/ODCampNavbar/ODCampNavbarContainer/ODCampNavbarLinkContainer/index.tsx
@@ -6,10 +6,12 @@ import Grid from '@material-ui/core/Grid';
 
 // ANCHOR Model
 import { MobileView } from '../../../../../models/mobile-view/MobileView';
+import { HamburgerToggle } from '../../../../../models/navbar/HamburgerToggle';
 
 // ANCHOR Component
 import { ODCampNavbarLinkItem } from './ODCampNavbarLinkItem';
 import { ODCampNavbarHamburger } from './ODCampNavbarHamburger';
+import { ODCampNavbarDrawer } from './ODCampNavbarDrawer';
 
 // ANCHOR Styles
 import { useStyles } from './styles';
@@ -25,7 +27,10 @@ export const ODCampNavbarLinkContainer = React.memo(() => {
   return (
     <Grid container className={classes.root}>
       {isMobile ? (
-        <ODCampNavbarHamburger />
+        <HamburgerToggle.Provider>
+          <ODCampNavbarHamburger />
+          <ODCampNavbarDrawer />
+        </HamburgerToggle.Provider>
       ) : (
         NAVBAR_LINK_CONSTANT.map(({ label, href }) => (
           <ODCampNavbarLinkItem label={label} href={href} />
